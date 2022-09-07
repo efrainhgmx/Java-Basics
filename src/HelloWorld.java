d839431d9184da276841cd7eb6052478c10de569
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-06 22:14:46 -0500

    Primitives values in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        
+        String greeting = "Hello World from Java!!!";
+        System.out.println(greeting);
+        System.out.println(greeting.toUpperCase());
+
+        int number = 10;
+        System.out.println(number);
+
+        boolean value = true;
+        if (value) {
+            System.out.println("Numero: " + number);
+        }
+
+        //This is very similar like JavaScript
+        var flexibleVariable = "3";
     }
 }
