ae92bc12ff0ef21eae48cc49e94669be8a437049
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-09 22:57:49 -0500

    boolean value

---
+++ src/PrimitivosBoolean.java
@@ -0,0 +1,18 @@
+public class PrimitivosBoolean {
+    public static void main(String[] args) {
+        //Es util para una estructura de control
+        boolean datoLogico = true;
+        boolean datoFalso = Boolean.FALSE.booleanValue();
+        System.out.println("datoLogico = " + datoLogico);
+        System.out.println("datoFalso = " + datoFalso);
+
+        double d = 84571e-3; //84.5713
+        float f = 1.23547e4f; // 12354.74
+
+        datoLogico = d > f;
+        System.out.println("Dato es = " + datoLogico);
+        
+        int valorUno = 125;
+        System.out.println("valorUno = " + valorUno);
+    }
+}
