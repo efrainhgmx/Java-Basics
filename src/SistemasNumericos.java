ed9bfd9ccf4608395731ce92dd7d535e13e5e4de
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-10 18:56:31 -0500

    Error exception and try catch

--- src/SistemasNumericos.java
+++ src/SistemasNumericos.java
@@ -1,22 +1,42 @@
+import javax.swing.*;
+
 public class SistemasNumericos {
     public static void main(String[] args) {
 
-        int numeroDecimal = 500;
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese el numero entero");
+        int numeroDecimal = 0;
+        try {
+            numeroDecimal = Integer.parseInt(numeroStr);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Debe colocar un numero entero valido");
+            main(args);
+            System.exit(0);
+        }
         System.out.println("numeroDecimal = " + numeroDecimal);
-        System.out.println("Numero Binario de = " + numeroDecimal + " es: "+ Integer.toBinaryString(numeroDecimal));
+        String resutaldoBinario = "Numero Binario de = " + numeroDecimal + " es: "+ Integer.toBinaryString(numeroDecimal);
+        System.out.println(resutaldoBinario);
 
         //Indica que es un binario al compilador de Java
         int numeroBinario = 0b111110100;
         System.out.println("numeroBinario = " + numeroBinario);
-        System.out.println("Numero Octal de 500 es: " + Integer.toOctalString(numeroBinario));
+        String resultadoOctal = "\nNumero Octal de = " + numeroDecimal + " es: "+ Integer.toOctalString(numeroDecimal);
+        System.out.println(resultadoOctal);
 
         //El 0 al inicio muestra inidca que es un octal
         int numeroOctal = 0764;
         System.out.println("numeroOctal = " + numeroOctal);
 
-        System.out.println("Numero Hexadecimal de 500: " + Integer.toHexString(numeroDecimal));
+        String resultadoHex = "\nNumero Hexadecimal de " + numeroDecimal + " es " + Integer.toHexString(numeroDecimal);
+
+        System.out.println(resultadoHex);
         
         int numeroHex = 0x1f4;
         System.out.println("numeroHex = " + numeroHex);
+
+        String mensaje = resutaldoBinario;
+        mensaje += resultadoOctal;
+        mensaje += resultadoHex;
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 }
