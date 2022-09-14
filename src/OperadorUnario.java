89198edcf168052a43b679de7ad8ff677737d298
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-13 20:27:39 -0500

    Unary operator

---
+++ src/OperadorUnario.java
@@ -0,0 +1,11 @@
+public class OperadorUnario {
+    public static void main(String[] args) {
+        int i = -5;
+        int j = +i;
+        int k = -j;
+
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        
+    }
+}
