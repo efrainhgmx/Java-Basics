e4bf870b335c5db37b6951621897993351a65d79
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-13 20:56:31 -0500

    Relational operator

---
+++ src/OperadorRelacional.java
@@ -0,0 +1,12 @@
+public class OperadorRelacional {
+    public static void main(String[] args) {
+        int i = 3;
+        int j = 7;
+
+        float k = 127e-7f;
+        double d  = 127.457e8;
+
+        boolean n = i != j;
+        System.out.println("n = " + n);
+    }
+}
