fc49bf8c2afae3137af02ef6433b8dccb2a6fffc
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-16 18:52:45 -0500

    Arrays in java

--- src/LoginEjemplo.java
+++ src/LoginEjemplo.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class LoginEjemplo {
     public static void main(String[] args) {
+        String[] usernames = new String[2];
+        String[] pws = new String[2];
         String username = "efrainhgmx";
         String password = "m123456l";
+
+        String username2 = "admin";
+        String password2 = "123456";
+
+        usernames[0] = username;
+        pws[0] = password;
+
+        usernames[1] = username2;
+        pws[1] = password2;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -15,10 +26,13 @@ public class LoginEjemplo {
 
         boolean estaLogeado = false;
 
-        if(uname.equals(username) && userpw.equals(password)) {
-            estaLogeado = true;
-        } else {
-            System.out.println("Lo siento usario y password malos intenta otravez!!");
+        for (int i = 0; i < usernames.length; i++){
+            if(usernames[i].equals(uname) && pws[i].equals(userpw)) {
+                estaLogeado = true;
+                break;
+            } else {
+                System.out.println("Lo siento usario y password malos intenta otravez!!");
+            }
         }
 
         if (!estaLogeado) {
