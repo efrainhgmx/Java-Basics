83996fb2a712b5a6986d752e46128dcb9328d5e7
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-16 19:04:33 -0500

    Ternary Operator is similar like in JavaScript

---
+++ src/Ternario.java
@@ -0,0 +1,12 @@
+public class Ternario {
+    public static void main(String[] args) {
+        //FUNCIONA IGUAL QUE EN JAVASCRIPT
+
+        int a = 7;
+        int b = 10;
+        String variable = 7 == 7 ? "Es verdadero" : "Es falso";
+
+        System.out.println(a + " es igual a" + b);
+        System.out.println(variable);
+    }
+}
