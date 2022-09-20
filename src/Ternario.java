5f9d4dc64560c5a70d97041913155fac2774e4db
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-19 20:39:41 -0500

    Other ternary expression

--- src/Ternario.java
+++ src/Ternario.java
@@ -4,9 +4,17 @@ public class Ternario {
 
         int a = 7;
         int b = 10;
+
         String variable = a == b ? "Es verdadero" : "Es falso";
 
         System.out.println(a + " es igual a" + b);
         System.out.println(variable);
+
+        int max = 100;
+        int min = 0;
+
+        String mensaje = (50 < max) && (50 > min ) ? "Valor valido" : "Valor fuera de rango";
+
+        System.out.println(mensaje);
     }
 }
