b1098c13fffb133a609da67a714e03c99f59a02c
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-19 20:28:41 -0500

    Ternary comparison updated

--- src/Ternario.java
+++ src/Ternario.java
@@ -4,7 +4,7 @@ public class Ternario {
 
         int a = 7;
         int b = 10;
-        String variable = 7 == 7 ? "Es verdadero" : "Es falso";
+        String variable = a == b ? "Es verdadero" : "Es falso";
 
         System.out.println(a + " es igual a" + b);
         System.out.println(variable);
