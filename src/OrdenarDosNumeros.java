07be20bce4e26b4e26a5e1c32088c005498070e3
Efrain Hernandez <efrainhg2050@gmail.com>
2022-09-26 22:51:24 -0500

    Order two numbers in java

---
+++ src/OrdenarDosNumeros.java
@@ -0,0 +1,24 @@
+import java.sql.Array;
+import java.util.Scanner;
+
+public class OrdenarDosNumeros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vamos a ordernar dos numeros de mayor a menor");
+
+        System.out.println("Dame el primer Numero: ");
+        int numberOne = scanner.nextInt();
+
+        System.out.println("Dame el segundo Numero: ");
+        int numberTwo = scanner.nextInt();
+
+        String order = "El orden es: ";
+
+        String ordenFinal = (numberOne >= numberTwo) 
+                ? order + numberOne + ", " + numberTwo 
+                : order +  numberTwo + ", " + numberOne;
+
+        System.out.println("ordenFinal = " + ordenFinal);
+
+    }
+}
